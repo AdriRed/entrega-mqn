c16544b91b579f4bc77f3be51036d0eb714d5f80
AdriRed <adriamilmarcos@gmail.com>
2025-04-15 18:41:21 +0200

    intent

--- entrega.f90
+++ entrega.f90
@@ -1,23 +1,26 @@
-!0.00433 700 0.015 1000000 0.00005 0.3 70000
-!0.00433 400 0.015 10000 0.0001 0.8 40000
-!0.00433 600 0.015 100000 0.0001 0.4 50000
-!0.00433 400 0.020 1000 0.0001 0.5 50000
-!0.00433 300 0.020 100 0.0001 0.5 50000 
-
 program practica9
    implicit none
    interface 
-      function initialize_wave_function(step, r, r2, width) result(psi)
+      function initialize_wave_function(step1, r1, r21, width1) result(psi)
          implicit none
          double precision, dimension(:), allocatable :: psi
-         double precision, intent(in):: step
-         double precision, dimension(width), intent(in):: r, r2
-         integer, intent(in):: width
+         double precision, intent(in):: step1
+         double precision, dimension(width1), intent(in):: r1, r21
+         integer, intent(in):: width1
       end function
+
+      subroutine compute_wave_equation(length, r1, r21, psi_pre, steps, h, psi_next)
+         implicit none
+      
+         integer, intent(in) :: length, steps
+         double precision, intent(in), dimension(length) :: psi_pre, r1, r21
+         double precision, intent(in) :: h
+         double precision, intent(out), dimension(length) :: psi_next
+      end subroutine compute_wave_equation
    end interface
 
    common /grosspita/alpha, alpha2, a0, cvar
-   common /simulation/step, time_step, atom_numbers
+   common /simulation/step, time_step, atom_numbers, interaction
    common /constants/pi
    double precision :: alpha, alpha2, cvar, pi, step, a0, time_step, interaction, density_param
    double precision, dimension(:), allocatable :: r, r2, prev_psi, next_psi
@@ -26,7 +29,7 @@ program practica9
    write(*,*) "a0, width, step, atom_numbers, time_step, alpha, iterations"
    read(*,*) a0, width, step, atom_numbers, time_step, alpha, iterations
 
-   allocate(r(width), r2(width))
+   allocate(r(width), r2(width), next_psi(width))
    
    pi = 4.0d0*datan(1.0d0)
 
@@ -52,6 +55,9 @@ program practica9
    end do
    close(1)
 
+   open(6, file="energy-per-iteration.dat")
+   call compute_wave_equation(width, r, r2, prev_psi, iterations, step, next_psi)
+   close(6)
 end program practica9
 
 function initialize_wave_function(step, r, r2, width) result(psi)
@@ -71,56 +77,85 @@ function initialize_wave_function(step, r, r2, width) result(psi)
 
 end function initialize_wave_function
 
-subroutine poisson_equation_algorithm(method_used, length, input_matrix, steps, rho, h, output_matrix)
+subroutine compute_wave_equation(length, r, r2, psi_pre, steps, h, psi_next)
    implicit none
-
-   interface
-      double precision function rho(x1)
-         integer, intent(in) :: x1
-      end function
-      subroutine method_used(width1, rho, h1, input_matrix1, output_matrix1)
-         implicit none
-         interface
-            double precision function rho(x1)
-               integer, intent(in) :: x1
-            end function
-         end interface
-         integer, intent(in) :: width1
-         double precision, intent(in) :: h1
-         double precision, intent(in), dimension(width1) :: input_matrix1
-         double precision, intent(out), dimension(width1) :: output_matrix1
-      end subroutine method_used
-   end interface
 
    integer, intent(in) :: length, steps
-   double precision, intent(in), dimension(length) :: input_matrix
+   double precision, intent(in), dimension(length) :: psi_pre, r, r2
    double precision, intent(in) :: h
-   double precision, intent(out), dimension(length) :: output_matrix
+   double precision, intent(out), dimension(:), allocatable :: psi_next
 
-   double precision, dimension(length) :: current_matrix
-   integer :: i
-
-   ! call method_used(length, rho, h, input_matrix, output_matrix)
-   current_matrix = input_matrix
-   output_matrix = 0
-
-   call method_used(length, rho, h, current_matrix, output_matrix)
-
-   do i = 1, steps
-      current_matrix = output_matrix
-      call method_used(length, rho, h, current_matrix, output_matrix)
-   end do
-end subroutine poisson_equation_algorithm
-
-subroutine jacobi_method(width, rho, h, input_matrix, output_matrix)
-   implicit none
 
    interface
-      double precision function rho(x, width1, input_matrix1)
-         integer, intent(in) :: x, width1
-         double precision, intent(in), dimension(width1) :: input_matrix1
-      end function
+      subroutine second_derivative(width, h1, input_matrix1, output_matrix1)
+         implicit none
+      
+         integer, intent(in) :: width
+         double precision, intent(in) :: h1
+         double precision, intent(in), dimension(width) :: input_matrix1
+         double precision, intent(out), dimension(width) :: output_matrix1
+      end subroutine second_derivative   
    end interface
+
+
+   double precision, dimension(length) :: psi_curr, psi_dx2, local_mu
+   integer :: i, j
+
+   double precision :: normalization_const, energy
+   common /simulation/step, time_step, atom_numbers, interaction
+   double precision :: step, time_step, atom_numbers, interaction
+
+   ! call method_used(length, rho, h, input_matrix, output_matrix)
+   psi_curr = psi_pre
+   psi_dx2 = 0
+
+
+   do j = 1, steps
+      call second_derivative(length, h, psi_curr, psi_dx2)
+
+      normalization_const = 0.0d0
+      energy = 0.0d0
+      
+      ! Cálculo de energía y potencial químico local
+      do i = 1, length       
+        if (i == 1) then
+         local_mu(i) = 0.0d0  ! Condición de contorno en r=0
+        else
+          ! Contribuciones a la energía
+          energy = energy - psi_curr(i)*psi_dx2(i)*0.5d0 + 0.5d0*r2(i)*psi_curr(i)*psi_curr(i) + &
+                 0.5d0*interaction*r2(i)*(psi_curr(i)/r(i))**4
+          
+          ! Potencial químico local
+          local_mu(i) = -0.5d0*psi_dx2(i)/psi_curr(i) + 0.5d0*r2(i) + interaction*(psi_curr(i)/r(i))**2
+        end if
+        
+        ! Evolución temporal de la función de onda
+        psi_next(i) = psi_curr(i) - time_step*local_mu(i)*psi_curr(i)
+        
+        ! Acumulación para normalización
+        normalization_const = normalization_const + psi_next(i)*psi_next(i)
+      end do
+      
+      ! Normalización y actualización
+      normalization_const = sqrt(normalization_const*step)
+      energy = energy*step
+      
+      ! Salida de información periódica
+      if (mod(j,200) == 0) then
+        write(6,*) 'Iteration:', j, ' Energy:', energy
+      end if
+      
+      ! Normalización y actualización de la función de onda
+      psi_curr = psi_next/normalization_const
+
+
+   end do
+end subroutine compute_wave_equation
+
+
+
+subroutine second_derivative(width, h, input_matrix, output_matrix)
+   implicit none
 
    integer, intent(in) :: width
    double precision, intent(in) :: h
@@ -131,11 +166,11 @@ subroutine jacobi_method(width, rho, h, input_matrix, output_matrix)
    ! output_matrix = input_matrix
 
    do i = 2, width-1
-      output_matrix(i) = (input_matrix(i+1) + input_matrix(i-1) - 2*input_matrix(i))/(h*h)
+      output_matrix(i) = (input_matrix(i+1) + input_matrix(i-1) - 2d0*input_matrix(i))/(h*h)
    end do
 
    output_matrix(width) = (input_matrix(width-1) - 2.0d0*input_matrix(width))/(h*h)
 
-end subroutine jacobi_method
+end subroutine second_derivative
 
 
