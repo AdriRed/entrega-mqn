78f075a45bc76d25083d8336ba4922529c4d3732
AdriRed <adriamilmarcos@gmail.com>
2025-04-18 00:48:55 +0200

    working fortran and started notebook

--- entrega.f90
+++ entrega.f90
@@ -1,4 +1,4 @@
-program practica9
+program entrega_mqn
    implicit none
    interface 
       function initialize_wave_function(step1, r1, r21, width1) result(psi)
@@ -9,32 +9,32 @@ program practica9
          integer, intent(in):: width1
       end function
 
-      subroutine compute_wave_equation(length, r1, r21, psi_pre, steps, h, psi_next, local_mu)
+      subroutine compute_wave_equation(length, r1, r21, psi_pre, steps, h, psi_next, local_mu1)
          implicit none
       
          integer, intent(in) :: length, steps
          double precision, intent(in), dimension(length) :: psi_pre, r1, r21
          double precision, intent(in) :: h
-         double precision, intent(out), dimension(length) :: psi_next, local_mu
+         double precision, intent(out), dimension(length) :: psi_next, local_mu1
       end subroutine compute_wave_equation
 
-      subroutine calculate_final_properties(length, r1, final_psi, local_mu)
+      subroutine calculate_final_properties(length, r1, final_psi, local_mu1)
          implicit none
       
          integer, intent(in) :: length
-         double precision, intent(in), dimension(length) :: final_psi, r1, local_mu
+         double precision, intent(in), dimension(length) :: final_psi, r1, local_mu1
       end subroutine calculate_final_properties
    end interface
    
-   common /grosspita/alpha, alpha2, a0, cvar
+   common /grosspita/alpha, alpha2, scattering_length, cvar
    common /simulation/step, time_step, atom_numbers, interaction
    common /constants/pi
-   double precision :: alpha, alpha2, cvar, pi, step, a0, time_step, interaction, density_param
+   double precision :: alpha, alpha2, cvar, pi, step, scattering_length, time_step, interaction, density_param
    double precision, dimension(:), allocatable :: r, r2, prev_psi, next_psi, local_mu
    integer :: width, i, atom_numbers, iterations
 
-   write(*,*) "a0, width, step, atom_numbers, time_step, alpha, iterations"
-   read(*,*) a0, width, step, atom_numbers, time_step, alpha, iterations
+   write(*,*) "scattering_length, width, step, atom_numbers, time_step, alpha, iterations"
+   read(*,*) scattering_length, width, step, atom_numbers, time_step, alpha, iterations
 
    allocate(r(width), r2(width), next_psi(width), local_mu(width))
    
@@ -42,19 +42,21 @@ program practica9
 
    !calcular constantes
    alpha2 = alpha*alpha
-   cvar = 2.0d0*dsqrt(alpha)**3/dsqrt(sqrt(pi))
-   ! interaction = 0
-   interaction = a0*atom_numbers
+   cvar = 2.0d0*dsqrt(alpha)**3/dsqrt(dsqrt(pi))
+   interaction = 0
+   ! interaction = scattering_length*atom_numbers
 
-   density_param = atom_numbers*a0*a0*a0
+   density_param = atom_numbers*scattering_length*scattering_length*scattering_length
 
    ! precompute spacial fields
+   write(*,*) "Precomputing spacial fields"
    do i = 1, width   
       r(i) = dfloat(i-1)*step
       r2(i) = r(i)*r(i)
    end do
 
    !inicializar funcion de onda (input_matrix)
+   write(*,*) "Initializing wave function"
    prev_psi = initialize_wave_function(step, r, r2, width)
 
    open(1, file="wave-function.dat")
@@ -63,13 +65,14 @@ program practica9
    end do
    close(1)
 
-   open(6, file="energy-per-iteration.dat")
-   call compute_wave_equation(width, r, r2, prev_psi, iterations, step, next_psi, local_mu)
-   close(6)
-
+   open(1, file="energy-per-iteration.dat")
+      write(*, *) "Computing wave equation"
+      call compute_wave_equation(width, r, r2, prev_psi, iterations, step, next_psi, local_mu)
+      write(*, *) "Computed wave equation"
+   close(1)
 
    call calculate_final_properties(width, r, next_psi, local_mu)
-end program practica9
+end program entrega_mqn
 
 function initialize_wave_function(step, r, r2, width) result(psi)
    implicit none
@@ -78,8 +81,8 @@ function initialize_wave_function(step, r, r2, width) result(psi)
    integer, intent(in):: width
    double precision, dimension(:), allocatable :: psi
    integer :: i
-   common /grosspita/alpha, alpha2, a0, cvar
-   double precision :: alpha, alpha2, a0, cvar
+   common /grosspita/alpha, alpha2, scattering_length, cvar
+   double precision :: alpha, alpha2, scattering_length, cvar
 
    allocate(psi(width))
    do i = 1, width
@@ -110,11 +113,11 @@ subroutine compute_wave_equation(length, r, r2, psi_pre, steps, h, psi_next, loc
 
    double precision, dimension(length) :: psi_curr, psi_dx2
    integer :: i, j
-
    double precision :: normalization_const, energy
+
    common /simulation/step, time_step, atom_numbers, interaction
    double precision :: step, time_step, atom_numbers, interaction
-
+   
    ! call method_used(length, rho, h, input_matrix, output_matrix)
    psi_curr = psi_pre
    psi_dx2 = 0
@@ -127,7 +130,8 @@ subroutine compute_wave_equation(length, r, r2, psi_pre, steps, h, psi_next, loc
       energy = 0.0d0
       
       ! Cálculo de energía y potencial químico local
-      do i = 1, length       
+      do i = 1, length
+
         if (i == 1) then
          local_mu(i) = 0.0d0  ! Condición de contorno en r=0
         else
@@ -152,13 +156,11 @@ subroutine compute_wave_equation(length, r, r2, psi_pre, steps, h, psi_next, loc
       
       ! Salida de información periódica
       ! if (mod(j,200) == 0) then
-      write(6,*) j, energy
+      write(1,*) j, energy
       ! end if
       
       ! Normalización y actualización de la función de onda
       psi_curr = psi_next/normalization_const
-
-
    end do
 end subroutine compute_wave_equation
 
@@ -200,9 +202,9 @@ subroutine calculate_final_properties(length, r, final_psi, local_mu)
    end interface
 
 
-   common /grosspita/alpha, alpha2, a0, cvar
+   common /grosspita/alpha, alpha2, scattering_length, cvar
    common /simulation/step, time_step, atom_numbers, interaction
-   double precision :: alpha, alpha2, cvar, a0, time_step, interaction, density_param, step
+   double precision :: alpha, alpha2, cvar, scattering_length, time_step, interaction, step
 
    integer :: i, atom_numbers
    double precision :: r2, total_energy, accumulted_radius
@@ -250,6 +252,7 @@ subroutine calculate_final_properties(length, r, final_psi, local_mu)
    armonic_potential = 0.5d0*armonic_potential*step
    interaction_energy = interaction_energy*step*interaction*0.5d0
    total_energy = kinetic_energy + armonic_potential + interaction_energy  ! Energía total
+   print *, "--------- Calculated properties ---------"
    write(*,'(/5X,"* ener",e12.5,"   average chemical=", e12.5/ &
    & 5X,"* kin-ener=",e12.5,"     total-pot= ", e12.5,/ &
    & 5X,"* potho=",E12.4,2X,"     potint =",E12.4/ &
